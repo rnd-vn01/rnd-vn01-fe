b47922d41f6dbfabf22ecffb913b237e1939b9fb
nguyencaonhan271201 <nguyencaonhan2712@gmail.com>
2022-12-30 00:42:29 +0700

    [c] Add loading scene with progress bar

--- src/components/ThreeJS/Body/Body.jsx
+++ src/components/ThreeJS/Body/Body.jsx
@@ -6,10 +6,10 @@ import MALEBODY from 'src/assets/models/NewSkinBody.glb';
 export const Body = () => {
   let mesh = useLoader(
     GLTFLoader,
-    MALEBODY
+    MALEBODY,
   );
-  
+
   return (
-    <primitive object={mesh.scene} rotation-y={-0.025} position={[0, -30, 0]}/>
+    <primitive object={mesh.scene} rotation-y={-0.025} position={[0, -30, 0]} />
   );
 };
