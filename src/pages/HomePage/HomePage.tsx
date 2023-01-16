33a6556c5e4300c0888b52d07e2bb5486c6c5320
nguyencaonhan271201 <nguyencaonhan2712@gmail.com>
2023-01-16 22:30:55 +0700

    [c] Fix tailwind config

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -8,7 +8,15 @@ export const HomePage: React.FC = () => {
       role="div"
       aria-label="home-page"
       className="home-page">
-        
+        <div className="grid grid-cols-5">
+          <div className="col-span-3">
+
+          </div>
+
+          <div className="col-span-2">
+            
+          </div>
+        </div>
     </div >
   );
 };
