cfb55a35ba34b05cf0e2aad903fb7c25915574c7
nguyencaonhan271201 <nguyencaonhan2712@gmail.com>
2023-01-29 23:36:33 +0700

    [f] Add meridian line

--- src/components/ThreeJS/Meridians/KI/KI.jsx
+++ src/components/ThreeJS/Meridians/KI/KI.jsx
@@ -4,7 +4,56 @@ import { BufferGeometry, Vector3 } from "three";
 
 export const KI = ({ }) => {
   const points = []
-  // points.push(new Vector3(0.35, 12.275, 2.5))
+  points.push(new Vector3(-4, -29.8, 1.25))
+  points.push(new Vector3(-3, -29.65, 1.25))
+  points.push(new Vector3(-2.45, -29, 0.25))
+  points.push(new Vector3(-2.35, -28.8, -0.2))
+  points.push(new Vector3(-2.325, -28.225, -0.4))
+  points.push(new Vector3(-2.3, -27.65, -1.2))
+  points.push(new Vector3(-2.375, -28, -1.5))
+  points.push(new Vector3(-2.34, -28.175, -1.35))
+  points.push(new Vector3(-2.35, -28.35, -1.2))
+  points.push(new Vector3(-2.35, -28.35, -0.7))
+  points.push(new Vector3(-2.25, -27.5, -1))
+  points.push(new Vector3(-2.4, -25.75, -1.75))
+  points.push(new Vector3(-2.35, -25.75, -1.45))
+  points.push(new Vector3(-2.4, -25.75, -1.15))
+  points.push(new Vector3(-2.2, -24.6, -1.5125))
+  points.push(new Vector3(-1.825, -23.65, -1.875))
+  points.push(new Vector3(-1.365, -22.55, -1.875))
+  points.push(new Vector3(-1.5, -21, -1.875))
+  points.push(new Vector3(-1.8, -19, -2.1))
+  points.push(new Vector3(-2.3, -17.5, -2.3))
+  points.push(new Vector3(-0.75, -14, -1.6))
+  points.push(new Vector3(-0.35, -12, -0.5))
+  points.push(new Vector3(-0.5, -11, 1.3))
+  points.push(new Vector3(-1.5, -10, 2.4))
+  points.push(new Vector3(-1.5, -8, 2.8))
+  points.push(new Vector3(-1.5, -7, 2.75))
+  points.push(new Vector3(-1.5, -5.9, 3.1))
+  points.push(new Vector3(-1.4, -5.5, 3.1))
+  points.push(new Vector3(-0.75, -4.325, 3.55))
+  points.push(new Vector3(-0.75, -3.7527, 3.575))
+  points.push(new Vector3(-0.75, -3.1755, 3.575))
+  points.push(new Vector3(-0.75, -2.6182, 3.525))
+  points.push(new Vector3(-0.75, -2.041, 3.45))
+  points.push(new Vector3(-0.75, -1.4636, 3.45))
+  points.push(new Vector3(-0.75, -0.85, 3.46))
+  points.push(new Vector3(-0.75, -0.3091, 3.315))
+  points.push(new Vector3(-0.75, 0.2682, 3.325))
+  points.push(new Vector3(-0.75, 0.555, 3.325))
+  points.push(new Vector3(-0.75, 0.8455, 3.25))
+  points.push(new Vector3(-0.75, 1.4227, 3.05))
+  points.push(new Vector3(-0.75, 2, 3.1))
+  points.push(new Vector3(-1.4, 2.5, 3))
+  points.push(new Vector3(-1.4, 3.125, 3.3))
+  points.push(new Vector3(-1.4, 3.625, 3.3))
+  points.push(new Vector3(-1.4, 4.098, 3.2))
+  points.push(new Vector3(-1.4, 4.5, 3.05))
+  points.push(new Vector3(-1.4, 5.07, 2.75))
+  points.push(new Vector3(-1.4, 5.5, 2.5))
+  points.push(new Vector3(-1.4, 6.042, 2.075))
+  points.push(new Vector3(-1.4, 6.864, 1.5))
 
   const lineGeometry = new BufferGeometry().setFromPoints(points)
 
@@ -22,22 +71,22 @@ export const KI = ({ }) => {
         labelPosition={0} />
 
       <Point
-        positionArray={[-2.35, -28.35, -0.7]}
+        positionArray={[-2.3, -27.65, -1.2]}
         label="KI-3"
         labelPosition={0} />
 
       <Point
-        positionArray={[-2.35, -28.35, -1.2]}
+        positionArray={[-2.375, -28, -1.5]}
         label="KI-4"
         labelPosition={0} />
 
       <Point
-        positionArray={[-2.375, -28, -1.5]}
+        positionArray={[-2.35, -28.35, -1.2]}
         label="KI-5"
         labelPosition={0} />
 
       <Point
-        positionArray={[-2.3, -27.65, -1.2]}
+        positionArray={[-2.35, -28.35, -0.7]}
         label="KI-6"
         labelPosition={0} />
 
