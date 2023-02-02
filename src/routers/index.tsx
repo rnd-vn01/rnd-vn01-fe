1dec84a130507428456b063a6e0cc74d96d02887
nguyencaonhan271201 <nguyencaonhan2712@gmail.com>
2023-02-03 01:30:24 +0700

    [b] Fix access level for about page

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -98,7 +98,7 @@ export function Routers() {
           path="/about"
           component={AboutPage}
           layout={FullLayout}
-          isPrivate={true}
+          isPrivate={false}
         />
 
         <BasicRoute
