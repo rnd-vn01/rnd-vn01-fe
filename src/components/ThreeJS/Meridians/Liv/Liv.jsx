aa09924f61fe79687033879f0b6a533f708b75ee
nguyencaonhan271201 <nguyencaonhan2712@gmail.com>
2023-02-11 02:03:28 +0700

    [f] Meridian line for Liv

--- src/components/ThreeJS/Meridians/Liv/Liv.jsx
+++ src/components/ThreeJS/Meridians/Liv/Liv.jsx
@@ -4,7 +4,34 @@ import { BufferGeometry, Vector3 } from "three";
 
 export const Liv = ({ }) => {
   const points = []
-  // points.push(new Vector3(-5.625, -29.4, 3.2))
+  points.push(new Vector3(4, -29.25, 4.4))
+  points.push(new Vector3(3.8, -29.05, 3.75))
+  points.push(new Vector3(3.55, -28.5, 2.75))
+  points.push(new Vector3(3.3, -28.15, 1.75))
+  points.push(new Vector3(3, -27.75, 0.7))
+  points.push(new Vector3(2.65, -27, 0))
+  points.push(new Vector3(2.7, -26, -0.35))
+  points.push(new Vector3(2.625, -25.1, -0.7))
+  points.push(new Vector3(2.525, -24.1, -0.7))
+  points.push(new Vector3(1.55, -22, -1.1))
+  points.push(new Vector3(1.6, -20.5, -1.3))
+  points.push(new Vector3(1.85, -19.25, -1.5))
+  points.push(new Vector3(1.95, -18.35, -1.55))
+  points.push(new Vector3(1.2, -16.35, -1.275))
+  points.push(new Vector3(0.8, -14.35, -1))
+  points.push(new Vector3(0.5, -13.85, 0))
+  points.push(new Vector3(0.8, -12.85, 1))
+  points.push(new Vector3(1, -10.85, 2))
+  points.push(new Vector3(1.25, -8.25, 2.8))
+  points.push(new Vector3(1.5, -7.45, 2.86))
+  points.push(new Vector3(1.5, -6.85, 2.9))
+  points.push(new Vector3(1.6, -4.75, 3.15))
+  points.push(new Vector3(2.5, -2.75, 2.75))
+  points.push(new Vector3(3.2, -1.75, 2.3))
+  points.push(new Vector3(3.4, -0.75, 1.5))
+  points.push(new Vector3(3.2, 0, 1.85))
+  points.push(new Vector3(3, 1, 2.365))
+  points.push(new Vector3(2.5, 1.45, 2.6))
 
   const lineGeometry = new BufferGeometry().setFromPoints(points)
 
@@ -63,7 +90,7 @@ export const Liv = ({ }) => {
       <Point
         positionArray={[1.5, -6.85, 2.9]}
         label="Liv-11"
-        labelPosition={3} />
+        labelPosition={2} />
 
       <Point
         positionArray={[1.6, -4.75, 3.15]}
--- src/components/ThreeJS/Scene/Scene.tsx
+++ src/components/ThreeJS/Scene/Scene.tsx
@@ -102,9 +102,9 @@ export const Scene: React.FC = () => {
       <KI />
       <PC />
       <SJ />
-      <GB /> */}
-      <Liv />
-      {/* <ST /> */}
+      <GB />
+      <Liv /> */}
+      <ST />
 
       {/* Floor */}
       <mesh rotation={[-(angleToRadians(90)), 0.02, 0]} position={[0, -29.9, 0]} receiveShadow>
