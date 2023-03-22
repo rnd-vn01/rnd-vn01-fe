66efa8c060f67561ac6ed08d322f35d90be96a94
nguyencaonhan271201 <nguyencaonhan2712@gmail.com>
2023-03-22 21:29:41 +0700

    [c] Change Data Management captions

--- src/i18n.ts
+++ src/i18n.ts
@@ -479,7 +479,7 @@ i18n
             placeholder: "Tìm kiếm..."
           },
           data_management_page: {
-            title: "Quản lý Dữ liệu",
+            title: "Cập nhật Dữ liệu",
           },
           placeholders: {
             name: "tên",
